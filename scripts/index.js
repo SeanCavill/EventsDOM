d9abaa0bc4dfdf53294b4fe143f64f668ebe7461
alikahwaji <ali.kahwaji@nmit.ac.nz>
2020-09-17 20:33:37 +1200

    Delete extra lines

--- scripts/index.js
+++ scripts/index.js
@@ -2,69 +2,53 @@ var button = document.getElementById("enter");
 var input = document.getElementById("input");
 var ul = document.querySelector("ul");
 
-
 function inputLength() {
-    return input.value.length;
+	return input.value.length;
 }
 
 function createListElement() {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
 
-    li.addEventListener("click", function () {
-        // creates a boolean that toggles the done class on li:
-        // if the list item is clicked this toggles the done class
-        var finished = this.classList.toggle("done");
-        // creates a remove button for the finished item:
-        var removeButton = document.createElement("button");
-        removeButton.classList.add("deleteButton");
+	li.addEventListener("click", function () {
+		// creates a boolean that toggles the done class on li:
+		// if the list item is clicked this toggles the done class
+		var finished = this.classList.toggle("done");
+		// creates a remove button for the finished item:
+		var removeButton = document.createElement("button");
+		removeButton.classList.add("deleteButton");
 
-        // if the list item is clicked (li add event listener ) then 
-        // finished is true
-        if (finished) {
-            removeButton.appendChild(document.createTextNode("remove"));
-            removeButton.classList = "deleteButton";
-            li.appendChild(removeButton);
+		// if the list item is clicked (li add event listener ) then
+		// finished is true
+		if (finished) {
+			removeButton.appendChild(document.createTextNode("remove"));
+			removeButton.classList = "deleteButton";
+			li.appendChild(removeButton);
 
-            removeButton.addEventListener("click", function () {
-                this.parentElement.remove();
-            });
-        } else {
-            this.getElementsByClassName("deleteButton")[0].remove();
-        }
-    })
-    // revert input value back to nothing
-    input.value = "";
+			removeButton.addEventListener("click", function () {
+				this.parentElement.remove();
+			});
+		} else {
+			this.getElementsByClassName("deleteButton")[0].remove();
+		}
+	});
+	// revert input value back to nothing
+	input.value = "";
 }
 
 function addListAfterClick() {
-    if (inputLength() > 0) {
-        createListElement();
-    }
+	if (inputLength() > 0) {
+		createListElement();
+	}
 }
 
 function addListAfterPress(event) {
-    if (inputLength() > 0 && event.keyCode === 13) {
-        createListElement();
-    }
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
 }
 
-// function listStrikethrough(event) {
-// 	var listItem = document.querySelector("li");
-// 	listItem.classList.toggle("done");
-// }
+button.addEventListener("click", addListAfterClick);
 
-// function deleteListItem(event) {
-// 	var itemToDelete = document.getElementById("list");
-// 	var myRemovedLink = myLinkList.lastChild;
-// 	myLinkList.removeChild(myRemovedLink);
-// }
-
-// li.addEventListener("click", listStrikethrough)
-
-// deleteButton.addEventListener("click", deleteListItem)
-
-button.addEventListener("click", addListAfterClick)
-
-input.addEventListener("keypress", addListAfterPress)
+input.addEventListener("keypress", addListAfterPress);
